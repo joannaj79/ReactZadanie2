f311ffe0f1093c306f9eab377e4c57219c2ad9d7
Joanna Jarzynska <jarzynskaj@gmail.com>
2019-11-03 18:44:36 +0100

    zmodyfikowalamscript.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 var Movie = React.createClass({
   propTypes: {
     image: React.PropTypes.object.isRequired,
@@ -8,8 +6,8 @@ var Movie = React.createClass({
   render: function() {
     return (
       React.createElement('div', {},
-        React.createElement('h2', {}, this.props.image.name),
-        React.createElement('img', {src: this.props.image.src})
+        React.createElement('h2', {}, this.props.image.title),
+        React.createElement('img', {src: this.props.image.img, style})
       )
     )
   },
@@ -19,36 +17,35 @@ var movies = [
   {
     id: 1,
     title: 'Harry Potter',
-img: 'https://bi.im-g.pl/im/91/1a/17/z24224401Q,Harry-Potter-i-Czara-Ognia.jpg',
+    img: 'https://bi.im-g.pl/im/91/1a/17/z24224401Q,Harry-Potter-i-Czara-Ognia.jpg',
     desc: 'Film o czarodzieju'
   },
   {
     id: 2,
     title: 'Król Lew',
-img: 'https://bi.im-g.pl/im/72/d4/17/z24987506V,Piesn-o-milosci--z-filmu--Krol-Lew--Audio-Only-.jpg',
+    img: 'https://bi.im-g.pl/im/72/d4/17/z24987506V,Piesn-o-milosci--z-filmu--Krol-Lew--Audio-Only-.jpg',
     desc: 'Film o królu sawanny'
   },
-	{
-id:3,
-title: 'Shrek',
-img: 'http://cdn1.thumbs.common.smcloud.net/common/2/4/s/24688590H9z.jpg/ru-1-r-640,0-n-24688590H9z.jpg',
-desc: 'Film o ogrze'
-}
+  {
+    id:3,
+    title: 'Shrek',
+    img: 'http://cdn1.thumbs.common.smcloud.net/common/2/4/s/24688590H9z.jpg/ru-1-r-640,0-n-24688590H9z.jpg',
+    desc: 'Film o ogrze'
+  }
 ];
 var style= {
-height: '200px',
-width: '200px'
+    height: '200px',
+    width: '200px'
 };
+
 var moviesElements = movies.map(function(movie) {
   return React.createElement('li', {key: movie.id},
-      React.createElement('h2', {}, movie.title),
-      React.createElement('img', {src:movie.img,style:style}),
-      React.createElement('p', {}, movie.desc)
+      React.createElement(Movie, {image:movie}) 
     );
 });
 
 var element =
-  React.createElement('div', {},
+    React.createElement('div', {},
     React.createElement('h1', {}, 'Lista filmów'),
     React.createElement('ul', {}, moviesElements)
   );
